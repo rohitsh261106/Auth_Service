f4ca22f413b97b0dc0414c8601c9539f441f6690
Rohit Sharma <rohit.sh2630@gmail.com>
2026-06-30 13:11:04 +0530

    Added function to compare the incoming password with encrypted password

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
-// const {User} = require('./models/index');
-// const bcrypt = require('bcrypt');
-const UserRepository = require('./repository/user-repository');
+//const UserService = require('./services/user-service');
 const app = express();
 
 const prepareAndStartServer = () => {
@@ -15,15 +13,12 @@ const prepareAndStartServer = () => {
 
   app.listen(PORT, async ()=>{
     console.log(`Server started on port ${PORT}`);
-    const repo = new UserRepository();
-    const response = await repo.getById(1);
-    console.log(response);
-
-
-    // const incomingpassword = 'Divya48583';
-    // const user = await User.findByPk(2);
-    // const response = bcrypt.compareSync(incomingpassword,user.Password);
-    // console.log(response);
+  // const service = new UserService();
+  //  const newToken = service.createToken({email:'rohit.sh7705@gmail.com',id:1});
+  //  console.log("new token is",newToken);
+  // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJvaGl0LnNoNzcwNUBnbWFpbC5jb20iLCJpZCI6MSwiaWF0IjoxNzgyODA0NDAzLCJleHAiOjE3ODI4MDgwMDN9.RMOlOOVH-cJjWbRWJUoPe1E7uX5VD_NNvNfyOU15QGo';
+ // const response = service.verifyToken(token);
+  //console.log(response);
   })
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const UserRepository = require('../repository/user-repository');
+const {JWT_KEY} = require('../config/serverConfig');
 
 class UserService {
   constructor() {
@@ -12,6 +15,38 @@ class UserService {
       
     } catch (error) {
       console.log("Something wnet wrong in service layer ");
+      throw error;
+      
+    }
+  }
+  createToken(user){
+    try {
+      const result = jwt.sign(user,JWT_KEY, {expiresIn:'1h'});
+      return result;
+    } catch (error) {
+      console.log("Something went wrong in token creation ");
+      throw error;
+      
+    }
+  }
+  verifyToken(token){
+    try {
+      const response = jwt.verify(token,JWT_KEY);
+      return response;
+      
+    } catch (error) {
+      console.log("Something went wrong in token validation",error);
+      throw error;
+      
+    }
+  }
+
+  checkPassword(userInputPlainPassword, encryptedPassword){
+    try {
+      return bcrypt.compareSync(userInputPlainPassword,encryptedPassword);
+      
+    } catch (error) {
+      console.log('Somthing went wrong in Password Comparison');
       throw error;
       
     }
