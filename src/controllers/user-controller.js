1704d470521828dd383fbec57879581a59fe52db
Rohit Sharma <rohit.sh2630@gmail.com>
2026-06-30 12:15:10 +0530

    Signup logic with password encrption

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,31 @@
+const {response} = require('express');
+const UserService = require('../services/user-service');
+
+const userService = new UserService();
+const create = async (req,res)=> {
+  try {
+    const response = await userService.create({
+      email:req.body.email,
+      Password:req.body.Password
+    });
+    return res.status(201).json({
+      success:true,
+      message:'Successfully created a new user',
+      data: response,
+      err: {}
+    });
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message:'something went wrong ',
+      data:{},
+      success: false,
+      err:error
+    });
+    
+  }
+}
+module.exports = {
+  create
+}
