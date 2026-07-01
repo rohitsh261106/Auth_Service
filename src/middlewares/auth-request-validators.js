912b96d44dabf42dd75290945c4798580665df9a
Rohit Sharma <rohit.sh2630@gmail.com>
2026-07-01 11:36:47 +0530

    Added user isAuth function

--- src/middlewares/auth-request-validators.js
+++ src/middlewares/auth-request-validators.js
@@ -1,14 +1,16 @@
-const validateUserAuth = (req,res,next)=> {
-  if(!req.body.email || req.body.Password){
+const validateUserAuth = (req, res, next) => {
+  if (!req.body.email || !req.body.Password) {
     return res.status(400).json({
-      success:false,
-      data:{},
-      message:'Something went wrong',
-      err:'Email or Password missing in the  request'
-    })
+      success: false,
+      data: {},
+      message: "Something went wrong",
+      err: "Email or Password missing in the request"
+    });
   }
+
   next();
-}
+};
+
 module.exports = {
   validateUserAuth
-}
+};
